e0df09912a1c73db06ff7a95c8fe7f04be8cd686
gamagamol <gamaaariefsadya@gmail.com>
2022-04-21 09:43:35 +0700

    benerin email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -20,7 +20,6 @@ class EmailController extends Controller
     public function email($no_tagihan)
     {
 
-        // dd('test');
 
         $no_tagihan = str_replace('-', '/', $no_tagihan);
         $data = $this->model->detail($no_tagihan);
@@ -49,52 +48,5 @@ class EmailController extends Controller
         return redirect()->to('bill')->with('email',"Email has been succes send to $subject");
     }
 
-    // jangan hapus dulu
-    // public function email($no_tagihan){
-    //     $no_tagihan = str_replace('-', '/', $no_tagihan);
-        
-    //     $data=[
-    //         'tittle'=>'Create Bill Payment Email',
-    //         'no_tagihan'=>$no_tagihan,
-            
-    //     ];
-    //   return view('mail.insert',$data);
-
-    // }
-
-    // public function store(Request $request){
-        
-    //     $request->validate([
-    //         'file'=> 'required|mimes:pdf'
-    //     ]);
-    //     $file=$request->file('file');
-
-    //     $no_tagihan=$request->input('no_tagihan');
-
-    //     $no_tagihan = str_replace('-', '/', $no_tagihan);
-    //     $data = $this->model->detail($no_tagihan);
-    //     $bill = $this->bc->bill_email($no_tagihan);
-        
-    //     // $path = storage_path('public/' . $file->getClientOriginalName());
-
-       
-
-    //     // CETAK PDF
-    //     $details = [
-    //         'perwakilan' => $data[0]->perwakilan,
-    //         'total' => $data[0]->total,
-    //         'data' => $bill['data'],
-
-    //     ];
-    //     Mail::to($data[0]->email)->send(new \App\Mail\Testmail($details, $file->getClientOriginalName()));
-
-    //     $subject = $data[0]->email;
-    //     return redirect()->to('bill')->with('email', "Email has been succes send to $subject");
-       
-
-        
-
-
-
-    // }
+    
 }
