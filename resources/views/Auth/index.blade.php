67b0a78880ff4548a5a2c2242633582d880d3a8d
gamagamol <gamaaariefsadya@gmail.com>
2022-04-28 06:44:16 +0700

    desain login

--- resources/views/Auth/index.blade.php
+++ resources/views/Auth/index.blade.php
@@ -26,56 +26,63 @@
 
     <div class="container">
 
-      
+
         <div class="row justify-content-center">
-           
+
             <div class="col-xl-10 col-lg-12 col-md-9">
-                
-                <div class="card o-hidden border-0 shadow-lg my-5 mt-5">
+
+                <div class="card o-hidden border-0 shadow-lg my-5 mt-5 " style="height:500px">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                             <div class="col-lg-6 d-none d-lg-block" style="background: url(http://127.0.0.1:8000/assets/img/bg-login.png); background-position: center; background-size: cover" ></div> 
-                            <div class="col">
+
+                                <div class="col-lg-6 d-none d-lg-block mt-5"
+                                    style="">
+                                    <img src="http://127.0.0.1:8000/assets/img/bg-login.png" alt="" width="500px">
+                                </div>
+                            <div class="col mt-5">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4" style="font-family: Arial, Helvetica, sans-serif;font-size: 2em">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4"
+                                            style="font-family: Arial, Helvetica, sans-serif;font-size:40px">Welcome Back!</h1>
                                         @if (session()->has('failed'))
-                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                            <strong>{{ session('failed') }}</strong>
-                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                              <span aria-hidden="true">&times;</span>
-                                            </button>
-                                          </div>
-                              
-                                     @endif
+                                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                                <strong>{{ session('failed') }}</strong>
+                                                <button type="button" class="close" data-dismiss="alert"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                        @endif
                                     </div>
                                     <form class="user" method="POST" action="login">
                                         @csrf
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user @error('ussername')is-invalid @enderror"
-                                              
-                                                placeholder="Enter Ussername" name="ussername">
-                                                @error('ussername')
+                                            <input type="text"
+                                                class="form-control form-control-user @error('ussername') is-invalid @enderror "
+                                                placeholder="Enter Ussername" name="ussername" style="margin:50px 0px 30px 0px">
+                                            @error('ussername')
                                                 <div class="invalid-feedback">
                                                     {{ $message }}
                                                 </div>
-                                                @enderror
+                                            @enderror
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user @error('password')is-invalid @enderror"
-                                                id="exampleInputPassword" placeholder="Password" name="password">
-                                                @error('password')
+                                            <input type="password"
+                                                class="form-control form-control-user @error('password') is-invalid @enderror "
+                                                id="exampleInputPassword" placeholder="Password" name="password" style="margin:30px 0px 50px 0px">
+                                            @error('password')
                                                 <div class="invalid-feedback">
                                                     {{ $message }}
                                                 </div>
-                                                @enderror
-                                            </div>
-                                  
-                                      <button type=submit name=submit class="btn btn-primary btn-user btn-block" style="font-size: 2em">submit</button>
-                                      
+                                            @enderror
+                                        </div>
+
+                                        <button type=submit name=submit
+                                            class="btn btn-primary btn-user btn-block" style="font-size:30px">submit</button>
+
                                     </form>
-                                   
+
                                 </div>
                             </div>
                         </div>
